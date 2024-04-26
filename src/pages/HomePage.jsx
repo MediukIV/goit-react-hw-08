16c16f33dfe3a248c3850d903dc18e6d9d27cc20
MediukIV <143748068+MediukIV@users.noreply.github.com>
2024-04-26 21:05:26 +0300

    add_start

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import Title from '../components/Title/Title';
+
+export default function HomePage() {
+  return (
+    <>
+      <Title>Home</Title>
+
+      <div>
+        <h1>
+          Welcome to our app{' '}
+          <span role="img" aria-label="Greeting icon">
+            🙋‍♂️
+          </span>
+        </h1>
+      </div>
+    </>
+  );
+}
